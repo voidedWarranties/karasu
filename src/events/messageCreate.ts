e553486d1795f34fe6bda49527d3bcc74fd37772
voidedWarranties <wz68.scratch@gmail.com>
2021-01-26 19:13:50 -0800

    Have bot respond to pings with the guild prefix

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -4,8 +4,15 @@ import Eris from "eris";
 
 export async function messageCreate(msg: Eris.Message) {
     const prefix = await this.resolvePrefix(msg);
+    const content = msg.content.replace("<@!", "<@");
 
-    const prefixUsed = prefix.find(p => msg.content.replace("<@!", "<@").toUpperCase().startsWith(p.toUpperCase()));
+    if (content.trim() === `<@${this.user.id}>`) {
+        await msg.channel.createMessage(`My prefix in this server is \`${prefix[0]}\``);
+
+        return;
+    }
+
+    const prefixUsed = prefix.find(p => content.toUpperCase().startsWith(p.toUpperCase()));
 
     if (prefixUsed) {
         const args = msg.content.slice(prefixUsed.length).match(/("[^\b"]*?")|('[^\b']*?')|([^\b\s]*)/g).filter(arg => arg !== "");
